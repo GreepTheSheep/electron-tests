3d48478fad62952cb56a43a4cbf80df80c7aa857
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-10-04 13:36:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const { app, BrowserWindow, Menu } = require('electron')
+
+
+const dockMenu = Menu.buildFromTemplate([
+  {
+    label: 'New Window',
+    click () {
+      console.log('New Window')
+    }
+  },
+  {
+    label: 'New Window with Settings',
+    submenu: [
+      { 
+        label: 'Basic'
+      },
+      { 
+        label: 'Pro'
+      }
+    ]
+  },
+  { 
+    label: 'New Command...'
+  }
+])
+
+function createWindow () {
+  // Create the browser window.
+    const window = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        darkTheme: false
+    })
+
+    //window.setMenu(null);
+    //app.dock.setMenu(dockMenu)
+    window.setMenu(dockMenu)
+
+    window.once('focus', () => window.flashFrame(false))
+    window.flashFrame(true)
+
+    // and load the index.html of the app.
+    window.loadFile('content/index.html')
+
+  
+    if (!window.isMaximized()) window.maximize()
+
+  // Open the DevTools.
+  window.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
